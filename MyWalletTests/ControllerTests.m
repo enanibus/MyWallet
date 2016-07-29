fa81a7859c558d355acf3612f00df6294c7eac49
enanibus <michi_two@yahoo.comn>
2016-07-30 01:45:31 +0200

    Clase 48. Implementación Tests de UITableViewControllers. TestThatHasOnlyOneSection OK

--- MyWalletTests/ControllerTests.m
+++ MyWalletTests/ControllerTests.m
@@ -8,11 +8,16 @@
 
 #import <XCTest/XCTest.h>
 #import "SimpleViewController.h"
+#import "WalletTableViewController.h"
+#import "Wallet.h"
 
 @interface ControllerTests : XCTestCase
 @property (nonatomic,strong) SimpleViewController *simpleVC;
 @property (nonatomic,strong) UIButton *button;
 @property (nonatomic,strong) UILabel *label;
+
+@property (nonatomic,strong) WalletTableViewController* walletVC;
+@property (nonatomic,strong) Wallet *wallet;
 
 @end
 
@@ -26,6 +31,12 @@
     [self.button setTitle:@"Hola" forState:UIControlStateNormal];
     self.label = [[UILabel alloc] initWithFrame:CGRectZero];
     self.simpleVC.displayLabel = self.label;
+    
+//    self.wallet = [[Wallet alloc] initWithAmount:1 currency:@"USD"];
+//    [self.wallet plus: [Money euroWithAmount:1]];
+//    
+    self.walletVC = [[WalletTableViewController alloc]
+                     initWithModel:self.wallet];
 
 }
 
@@ -44,5 +55,17 @@
     XCTAssertEqualObjects(self.button.titleLabel.text, self.label.text,@"Button and label should have the same text");
     
 }
+
+-(void) testThatTableHasOneSection{
+    NSInteger sections = [self.walletVC numberOfSectionsInTableView: nil];
+    
+    XCTAssertEqual(sections, 1, @"There can only by one");
+    
+}
+
+//-(void) testThatTableNumberOfCellsIsNumberOfMoneysPlusOne{
+//    XCTAssertEqual(self.wallet.count+1, [self.walletVC tableView:nil numberOfRowsInSection:0],@"Number of cells is the number of moneys plus 1 (the total)");
+//}
+
 
 @end
---
+++ WalletTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WalletTableViewController.h
+//  MyWallet
+//
+//  Created by Jacobo Enriquez Gabeiras on 30/7/16.
+//  Copyright © 2016 enanibus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Wallet;
+
+@interface WalletTableViewController : UITableViewController
+
+-(id) initWithModel:(Wallet *) model;
+
+@end
